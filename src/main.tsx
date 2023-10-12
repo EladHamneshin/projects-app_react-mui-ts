be69b58e00971dfbc679be0442522e643ccbcfce
Elad Hamneshin <hamneshin123@gmail.com>
2023-10-12 12:37:14 +0300

    {update} delete context provuder from maintsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import ProjectContextProvider from './contexts/ProjectContextProvider.tsx'
 import router from './routes/Router.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
@@ -9,10 +8,8 @@ import { store } from './store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <ProjectContextProvider>
         <Provider store={store}>
           <RouterProvider router={router}/>
         </Provider>
-      </ProjectContextProvider>
   </React.StrictMode>,
 )
